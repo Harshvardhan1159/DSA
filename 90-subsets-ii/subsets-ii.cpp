ff690bd7a024fb244119fd96878699e9328ea2a8
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-02-11 12:06:23 +0530

    Time: 3 ms (39.02%) | Memory: 13.4 MB (22.00%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+void func(vector<int>&nums,vector<vector<int>>&ans,vector<int>& temp,int idx,int n, map<int,int>&mp){
+    if(idx==n){
+        ans.push_back(temp);
+        return;
+    }
+    int x = mp[nums[idx]];
+    func(nums,ans,temp,idx+1,n,mp);
+    for(int i=0;i<x;i++){
+     temp.push_back(nums[idx]);    //included
+     func(nums,ans,temp,idx+1,n,mp);
+    
+    }
+    for(int i=0;i<x;i++){
+        temp.pop_back();
+    }
+
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>temp;
+        vector<vector<int>>ans;
+       map<int,int>mp;
+        int n = nums.size();
+        for(int i =0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        vector<int>dem;
+        for(auto i:mp){
+            dem.push_back(i.first);
+        }
+        func(dem,ans,temp,0,dem.size(),mp);
+        return ans;
+
+    }
+};
