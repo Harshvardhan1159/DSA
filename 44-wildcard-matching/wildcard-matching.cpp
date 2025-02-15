27304d4730b87b2b1fcae411fa0c40338d7fc9e6
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-02-15 18:40:42 +0530

    Time: 40 ms (20.47%) | Memory: 14.9 MB (61.34%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -45,8 +45,26 @@ bool allstar(string p, int j){
 
    }
     bool isMatch(string s, string p) {
-     vector<vector<int>> dp(s.size(), vector<int>(p.size(), -1));
+     vector<vector<bool>> dp(s.size()+1, vector<bool>(p.size()+1, false));
+     dp[0][0]=true;
+     for(int i=1;i<=p.size();i++){
+        if(p[i-1]=='*')dp[0][i]=dp[0][i-1];
+     }
+     for(int i=1;i<=s.size();i++){
+        dp[i][0]=false;
+     }
+     for(int i=1;i<=s.size();i++){
+        for(int j = 1;j<=p.size();j++){
+            if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                dp[i][j]=dp[i-1][j-1];
+            }else if(p[j-1]=='*'){
+                dp[i][j]=dp[i][j-1]||dp[i-1][j];
+            }else{
+                dp[i][j]=false;
+            }
+        }
+     }
 
-     return func(s,p,0,0,dp);
+     return dp[s.size()][p.size()];
     }
 };
