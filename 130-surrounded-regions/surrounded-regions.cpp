d347c2093de2f24e149278542c441ef00bea9f52
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-02-20 18:09:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (90.79%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -5,7 +5,7 @@ public:
     
       if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != 'O') return;
 
-     grid[i][j]='Y';
+     grid[i][j]='Y';  //mark the o connected cells from the boundary
      if(i+1<n){
         dfs(grid,i+1,j,n,m);
      }
@@ -27,7 +27,7 @@ public:
         if(board[0][i]=='O'){
             dfs(board,0,i,n,m);
         }
-    }
+    }                       //seedha border element se check krlo jo accesbile hoge vo convert nhi hoge X me
     for(int i =0;i<m;i++){
         if(board[n-1][i]=='O'){
              dfs(board,n-1,i,n,m);
@@ -45,7 +45,7 @@ public:
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(board[i][j]=='Y'){
+            if(board[i][j]=='Y'){     //jo border se dfs ho gye the unhe vapis y se o krdiyaa
                 board[i][j]='O';
             }else{
                 board[i][j]='X';
