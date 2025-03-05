604cd8247c998c68a56cdc65a79a0904af0bbafa
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-03-05 08:22:45 +0530

    Time: 2 ms (15.34%) | Memory: 12 MB (28.82%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -6,14 +6,27 @@ public:
         if(v[i][j]==1)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
         int x = (i-1>=0)?func(v,i-1,j,dp):0;
-        int y = (y-1>=0)?func(v,i,j-1,dp):0;
+        int y = (j-1>=0)?func(v,i,j-1,dp):0;
         return dp[i][j]= x+y;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& v) {
         int n= v.size();
         int m = v[0].size();
         if(v[0][0]==1)return 0;
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return func(v,n-1,m-1,dp);
+        vector<vector<int>>dp(n,vector<int>(m));
+        dp[0][0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0)continue;
+                if(v[i][j]==1)dp[i][j]=0;
+                else{ 
+                 int x = (i-1>=0)?dp[i-1][j]:0;
+                 int y = (j-1>=0)?dp[i][j-1]:0;
+                   dp[i][j]= x+y;
+                }
+
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
