8ede3b23fa0485cee73291b2eaefb80cd1899537
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-03-06 08:30:54 +0530

    Time: 3 ms (30.03%) | Memory: 16.5 MB (13.81%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int func(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
         if (i == 0 && j == 0) return grid[0][0];
-        if (i < 0 || j < 0) return 1e9; // A large number to avoid incorrect min calculation
+        if (i < 0 || j < 0) return 1e9; 
         if (dp[i][j] != -1) return dp[i][j];
 
         int up = grid[i][j] + func(i - 1, j, grid, dp);
@@ -14,7 +14,18 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return func(n - 1, m - 1, grid, dp);
+        vector<vector<int>> dp(n, vector<int>(m));
+        dp[0][0]=grid[0][0];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0)continue;
+             int x = (i > 0) ? grid[i][j]+dp[i-1][j] : 1e9; 
+                int y = (j > 0) ?grid[i][j]+ dp[i][j-1] : 1e9;
+                 dp[i][j]=min(x,y);
+   
+            }
+        }
+       return dp[n-1][m-1];
     }
+       
 };
