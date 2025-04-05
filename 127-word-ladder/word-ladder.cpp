4d0b56fc7820b62372d66a9b5444f521f8f2f43a
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-04-05 12:20:03 +0530

    Time: 51 ms (82.54%) | Memory: 21 MB (70.61%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,40 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int ladderLength(string begin, string end, vector<string>& word) {
-        unordered_set<string> s(word.begin(), word.end());
-        
-        if (s.find(end) == s.end()) return 0; 
-
-        queue<pair<string, int>> q;
-        q.push({begin, 1}); 
-
-        while (!q.empty()) {
-            pair<string, int> p = q.front();
-            q.pop();
-
-            string src = p.first;
-            int step = p.second;
-
-            if (src == end) {
-                return step;
-            }
-
-            for (int i = 0; i < src.size(); i++) {
-                string temp = src; 
-                for (char it = 'a'; it <= 'z'; it++) {
-                    temp[i] = it;
-
-                    if (s.count(temp)) {
-                        s.erase(temp);
-                        q.push({temp, step + 1});
-                    }
-                }
-            }
+    int ladderLength(string st, string e, vector<string>& word) {
+        unordered_set<string>s(word.begin(),word.end());
+        queue<pair<string,int>>q;
+        q.push({st,1});
+        while(!q.empty()){
+        pair<string,int>p=q.front();
+        q.pop();
+        string x = p.first;
+        int c = p.second;
+        if(x==e)return c;
+        for(int i=0;i<x.size();i++){
+            string temp =x;
+        for(char it='a';it<='z';it++){
+          temp[i]=it;
+          if(s.find(temp)!=s.end()){
+            q.push({temp,c+1});
+            s.erase(temp);
+          }  
         }
-
+        }
+        }
         return 0;
     }
 };
