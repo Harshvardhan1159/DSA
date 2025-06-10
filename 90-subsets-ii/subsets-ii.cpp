088048918f5e04c6e363faf368c6b33a5ebd1185
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-06-10 16:26:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (68.73%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-void func(vector<int>&nums,vector<vector<int>>&ans,vector<int>& temp,int idx,int n, unordered_map<int,int>&mp){
-    if(idx==n){
-        ans.push_back(temp);
-        return;
-    }
-    int x = mp[nums[idx]];
-    func(nums,ans,temp,idx+1,n,mp);
-    for(int i=0;i<x;i++){
-     temp.push_back(nums[idx]);    //included
-     func(nums,ans,temp,idx+1,n,mp);
+void f(int idx,vector<vector<int>>&ans,vector<int>&dem,vector<int>& nums){
     
+        ans.push_back(dem);
+    
+    for(int i = idx;i<nums.size();i++){
+        if(i>idx && nums[i]==nums[i-1])continue;
+        dem.push_back(nums[i]);
+        f(i+1,ans,dem,nums);
+        dem.pop_back();
     }
-    for(int i=0;i<x;i++){
-        temp.pop_back();
-    }
-
 }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int>temp;
         vector<vector<int>>ans;
-       unordered_map<int,int>mp;
-        int n = nums.size();
-        for(int i =0;i<n;i++){
-            mp[nums[i]]++;
-        }
         vector<int>dem;
-        for(auto i:mp){
-            dem.push_back(i.first);
-        }
-        func(dem,ans,temp,0,dem.size(),mp);
+        sort(nums.begin(),nums.end());
+        f(0,ans,dem,nums);
         return ans;
-
     }
 };
