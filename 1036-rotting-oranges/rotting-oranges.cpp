1dc1961f9c12bd44c9b35925cda5e608094b8cb9
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-07-04 16:55:17 +0530

    Time: 3 ms (30.26%) | Memory: 17.2 MB (34.39%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,49 +1,57 @@
 class Solution {
 public:
+    int bfs(vector<vector<int>>& grid, vector<vector<int>>& vis) {
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int, pair<int, int>>> q;
+        int drow[4] = {-1, 0, 1, 0};
+        int dcol[4] = {0, 1, 0, -1};
+
+        // Push all initially rotten oranges
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 2) {
+                    q.push({0, {i, j}});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        int lvl = 0;
+        while(!q.empty()) {
+            auto [time, cell] = q.front();
+            int r = cell.first;
+            int c = cell.second;
+            q.pop();
+            lvl = max(lvl, time);
+
+            for(int k = 0; k < 4; k++) {
+                int nr = r + drow[k];
+                int nc = c + dcol[k];
+
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1 && vis[nr][nc] == -1) {
+                    q.push({time + 1, {nr, nc}});
+                    vis[nr][nc] = 1;
+                    grid[nr][nc] = 2;
+                }
+            }
+        }
+
+        // Check for any fresh orange left
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1)
+                    return -1;
+            }
+        }
+
+        return lvl;
+    }
+
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<pair<int,int>,int>>q;
-        vector<vector<bool>>vis(n,vector<bool>(m,false));
-        for(int i=0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    vis[i][j]=true;
-                }
-            }
-        }
-        int drow[4] = {-1,0,1,0};
-        int dcol[4] = {0,+1,0,-1};
-        int step =0;
-        while(!q.empty()){
-        pair<pair<int,int>,int>p= q.front();
-        q.pop();
-        int i = p.first.first;
-        int j = p.first.second;
-         step = p.second;
-       
-        vis[i][j]=true;
-        for(int k=0;k<4;k++){
-            int ni = i+drow[k];
-            int nj = j+dcol[k];
-            if(ni<n && ni>=0  && nj>=0 && nj<m && grid[ni][nj]==1 && !vis[ni][nj]){
-                q.push({{ni,nj},step+1});
-                vis[ni][nj]=true;
-                 grid[ni][nj]=2;
-            }
-        }
-
-        }
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
-            }
-        }
-        return step;
-
+        vector<vector<int>> vis(n, vector<int>(m, -1));
+        return bfs(grid, vis);
     }
 };
