59481eb13204c2556afd531782e1d9fa0ad122a1
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-07-21 13:08:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (55.41%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,44 +1,34 @@
 class Solution {
-
-    private:
-    int valid(int mid,vector<int>& nums,int n){
-        int k =1;
-        int sum=0;
-        for(int i=0;i<n;i++){
-          if(nums[i]+sum<=mid){
-            sum+=nums[i];
-
-          }else{
-            sum=nums[i];
-            k++;
-          }
-        }
-        return k;
-    }
 public:
-    int splitArray(vector<int>& nums, int k) {
-       int sum = 0;
-       int n = nums.size();
-       int s =0;
-       for(int i=0;i<n;i++){
-        sum+=nums[i];
-        s= max(nums[i],s);
-       } 
-    
-    int ans =0;
-    int e = sum;
-     while(s<=e){
-    int mid = s+(e-s)/2;
-    int split = valid(mid,nums,n);
-    if(split<=k){
-       ans =mid;
-        e=mid-1;
-    }else{
-        s= mid+1;
+    bool possible(vector<int>& nums, int k, int mid) {
+        int cnt = 1, sum = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(sum + nums[i] <= mid) {
+                sum += nums[i];
+            } else {
+                cnt++;
+                if(cnt > k) return false;
+                sum = nums[i];
+            }
+        }
+        return true;
     }
-     
 
-     }
-     return ans;
+    int splitArray(vector<int>& nums, int k) {
+        int e = *max_element(nums.begin(), nums.end());  // fix lower bound
+        int h = 0;
+        for(int x : nums) h += x;
+        int ans = -1;
+
+        while(e <= h) {
+            int mid = e + (h - e) / 2;
+            if(possible(nums, k, mid)) {
+                ans = mid;
+                h = mid - 1;  // try smaller largest sum
+            } else {
+                e = mid + 1;
+            }
+        }
+        return ans;
     }
 };
