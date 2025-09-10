554635d3b9e70b6cbd2338972afce4b47fb51907
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-09-10 14:37:13 +0530

    Time: 68 ms (7.03%) | Memory: 31.7 MB (15.98%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    int lcs(string t, string s, vector<vector<int>>&dp){
-        int n = s.size();
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= n; j++){
-                if(i == 0 || j == 0)
-                dp[i][j] = 0;
-            }
-        }
+    int f(int n, int m, string &s, string &x,vector<vector<int>>&dp) {
+        if (n == 0 || m == 0) return 0;
+        if(dp[n][m]!=-1)return dp[n][m];
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                if(s[i-1] == t[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+        if (s[n-1] == x[m-1]) {
+            return dp[n][m]=1 + f(n - 1, m - 1, s, x,dp);
+        } else {
+            return dp[n][m]=max(f(n - 1, m, s, x,dp), f(n, m - 1, s, x,dp));
         }
-        return dp[n][n];
     }
-    
+
     int minInsertions(string s) {
-        string t = s;
-        reverse(t.begin(),t.end());
+        string x = s;
+        
+        reverse(x.begin(), x.end());
+     
         int n = s.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        return n - lcs(s,t,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+
+        int lcs = f(n, n, s, x,dp);
+
+        return n - lcs;
     }
 };
