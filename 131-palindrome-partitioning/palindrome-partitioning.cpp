b52e1d0ae6dc596c569140dfaee5338317c0adea
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2025-10-14 17:59:54 +0530

    Time: 117 ms (14.65%) | Memory: 140.8 MB (12.60%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-  bool ispalindrome(string str) {
-    int left = 0, right = str.length() - 1;
-    while (left < right) {
-        if (str[left++] != str[right--])
-            return false;
+   bool pal(string s){
+    int i=0;
+    int j=s.size()-1;
+    while(i<=j){
+        if(s[i]!=s[j])return false;
+        i++;
+        j--;
     }
     return true;
-}
-void func (string s,vector<vector<string>>&ans,vector<string>&temp,int idx , int n){
-    if(idx==n){
-        ans.push_back(temp);
-        return;
-    }
-    for(int i=idx;i<n;i++){
-        if(ispalindrome(s.substr(idx,i-idx+1))){
-            temp.push_back(s.substr(idx,i-idx+1));
-            func(s,ans,temp,i+1,n);
-            temp.pop_back();
+   }
+    void f(string &s,int i,int n, vector<vector<string>>&ans,vector<string>temp){
+        if(i==n){
+            ans.push_back(temp);
+            return;
         }
+
+        for(int j=i;j<n;j++){
+            if(pal(s.substr(i,j-i+1))){
+                string x=s.substr(i,j-i+1);
+                temp.push_back(x);
+                f(s,j+1,n,ans,temp);
+                temp.pop_back();
+                
+            }
+        }
+        
     }
-}
     vector<vector<string>> partition(string s) {
-        int n = s.size();
-        vector<vector<string>>ans;
-        vector<string>temp;
-        func(s,ans,temp,0,n);
-        return ans;
+         vector<vector<string>>ans;
+         vector<string>temp;
+         int n = s.size();
+         f(s,0,n,ans,temp);
+         return ans;
     }
 };
